f2b658c7b0f104d2cdfc1b80f9f5be33e63c704e
Sergio Oliveira <srsoliveira@gmail.com>
2011-08-12 04:26:44 -0300

    Remoção da página padrão do Rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # Retorna um título em uma página de base
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    
+    if @title.nil?
+      base_title
+
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
